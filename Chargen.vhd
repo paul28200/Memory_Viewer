7155dc078e562c6a3a723fd942c123cd5dbdcab5
paul28200 <60981460+paul28200@users.noreply.github.com>
2025-06-12 17:33:54 +0200

    Update Chargen.vhd
    Add clock enable input

--- Chargen.vhd
+++ Chargen.vhd
@@ -1,36 +1,10 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    14:39:54 06/04/2013 
--- Design Name: 
--- Module Name:    Chargen - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Chargen is
-    Port ( clk_25MHz : in  STD_LOGIC;
+    Port ( clk, ce : in  STD_LOGIC;	-- 25MHz
            pixel_col : in  STD_LOGIC_VECTOR (2 downto 0);
            pixel_line : in  STD_LOGIC_VECTOR (4 downto 0);
            graphic : in  STD_LOGIC;
@@ -49,6 +23,7 @@ architecture Behavioral of Chargen is
 	COMPONENT ROM_Char
 	  PORT (
 		 clka : IN STD_LOGIC;
+		 ena : IN STD_LOGIC;
 		 addra : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
 		 douta : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
 	  );
@@ -57,7 +32,8 @@ architecture Behavioral of Chargen is
 begin
 
 Inst_Rom_Chargen : Rom_Char port map (
-    clka => clk_25MHz,
+    clka => clk,
+    ena => ce,
     addra(10) => graphic,
 	 addra(9 downto 3) => displayed_character(6 downto 0),
 	 addra(2 downto 0) => pixel_line (3 downto 1),
@@ -65,17 +41,18 @@ Inst_Rom_Chargen : Rom_Char port map (
   );
 
 
-process(clk_25MHz)		--Process pour synchro du signal VGA
+process(clk)		--Process pour synchro du signal VGA
 begin
-	if (clk_25MHz'event and clk_25MHz='1') then
-		reverse <= displayed_character(7);	--Place dans reverse pour tre synchrone avec la lecture de ROM Chargen
-		if VGA_OUT_enable = '0' or pixel_line(4)= '1' then
-			VGA_OUT <= '0';
-		else
-			VGA_OUT <= displayed_8_pixels(to_integer(unsigned(not pixel_col(2 downto 0)))) xor reverse;
+	if (clk'event and clk = '1') then
+		if ce = '1' then
+			reverse <= displayed_character(7);	--Place dans reverse pour tre synchrone avec la lecture de ROM Chargen
+			if VGA_OUT_enable = '0' or pixel_line(4)= '1' then
+				VGA_OUT <= '0';
+			else
+				VGA_OUT <= displayed_8_pixels(to_integer(unsigned(not pixel_col(2 downto 0)))) xor reverse;
+			end if;
 		end if;
 	end if;
 end process;
 
 end Behavioral;
-
